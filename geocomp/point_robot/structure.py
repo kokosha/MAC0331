5d775d5b25cb44d941fe678c2b7fc7a983d93939
Jiang Zhi <jiang.zhi@usp.br>
2018-11-21 21:43:52 -0200

    Python que linguagem ruim para ABBT

--- geocomp/point_robot/structure.py
+++ geocomp/point_robot/structure.py
@@ -1,4 +1,5 @@
 import random
+
 class SPoint():
     def __init__ (self, x = None, y = None):
         self.x = x
@@ -23,11 +24,12 @@ class SSegment():
         return ccw(self.p_left, self.p_right, p) > 0; 
 
 class STrapezoid():
-    def __init__ (self, p_left = None, p_right = None, s_top = None, s_bottom = None):
+    def __init__ (self, p_left = None, p_right = None, s_top = None, s_bottom = None, vid = None):
         self.p_left = p_left
         self.p_right = p_right
         self.s_top = s_top
         self.s_bottom = s_bottom
+        self.vid = vid
 
         self.t_upper_left = None
         self.t_lower_left = None
@@ -38,12 +40,13 @@ class STrapezoid():
 
 
 class SNode():
-    def __init__ (self, left = None, right = None, node_type = None, info = None):
+    def __init__ (self, left = None, right = None, node_type = None, info = None, vid = None):
         self.left = left
         self.right = right
         # 0 - trapezio, 1 - segmento, 2 - ponto
         self.node_type = node_type
         self.info = info
+        self.vid = vid
     def query(p_p):
         at = self
         while at.node_type != 0 :
@@ -65,6 +68,7 @@ class SNode():
 class STrapezoidMap():
     def __init__(self, segments):
         self.segments = segments
+        self.trapezoid_number = 0
 
         # Achando o trapezio externo
         minX = 1e9
@@ -88,13 +92,21 @@ class STrapezoidMap():
         s_bottom = SSegment(SPoint(minX, minY), SPoint(maxX, minY))
         p_left = SPoint(minX, maxY)
         p_right = SPoint(maxX, maxY)
-        t_start = STrapezoid(p_left, p_right, s_top, s_bottom);
+        t_start = STrapezoid(p_left, p_right, s_top, s_bottom, 0)
 
-        self.root = SNode(None, None, 0, t_start);
+
+        # Estrutura de Busca
+        self.node_list = [SNode(None, None, 0, t_start, 0)]
+
+        # Posicao de guardar
+        self.trapezoid_list = [t_start]
+        self.trapezoid_number++;
+
 
         random.shuffle(segments)
         for seg in segments:
             add(seg)
+
 
     def follow_segment(segment):
         # Let p and q be the left and right endpoint of si.
@@ -134,79 +146,101 @@ class STrapezoidMap():
             # Então pode acontecer de ter 1 trapezios
 
             t = node.info
+            # REMOVER TRAP LIST DEPOIS..
 
             # Trecho 1 - Atualizar o mapa trapezoidal
             # Trecho 1.1 - Parte de botar os segmentos e pontos dos trapézios no lugar certo
+            
             # FUTURO CORNER CASE
             t_left = node.info
+            self.trapezoid = self.trapezoid + 1
+            t_left.vid = self.trapezoid
             t_left.pright = segment.s_left
+            self.trapezoid_list.append(t_left)
 
             # FUTURO CORNER CASE
             t_right = node.info
+            self.trapezoid = self.trapezoid + 1
+            t_right.vid = self.trapezoid
             t_right.p_left= segment.s_right 
+            self.trapezoid_list.append(t_right)
 
             t_bottom = node.info
+            self.trazezoid = self.trapezoid + 1
+            t_bottom.vid = self.trapezoid
             t_bottom.s_top = segment;
             t_bottom.p_right = segment.s_left
             t_bottom.p_left = segment.s_right
+            self.trapezoid_list.append(t_bottom)
+
 
             t_top = node.info
+            self.trapezoid = self.trapezoid + 1
+            t_top.vid = self.trapezoid
             t_top.s_bot = segment;
             t_top.p_right = segment.s_left
             t_top.p_left = segment.s_right
+            self.trapezoid_list.append(t_top)
 
 
             # Trecho 1.2 - Parte de botar as relações dos trapézios no lugar certo
 
-            t_left.t_upper_left = t.t_upper_left
-            t_left.t_lower_left = t.t_lower_left
-            t_left.t_upper_right = t_top
-            t_left.t_lower_right = t_bottom
+            t_left.t_upper_left = t.t_upper_left.vid
+            t_left.t_lower_left = t.t_lower_left.vid
+            t_left.t_upper_right = t_top.vid
+            t_left.t_lower_right = t_bottom.vid
 
-            t_right.t_upper_left = t_top
-            t_right.t_lower_left = t_bottom
-            t_right.t_upper_right = t.t_upper_right
-            t_right.t_lower_right = t.t_lower_right
+            t_right.t_upper_left = t_top.vid
+            t_right.t_lower_left = t_bottom.vid
+            t_right.t_upper_right = t.t_upper_right.vid
+            t_right.t_lower_right = t.t_lower_right.vid
 
-            t_bottom.t_upper_left = t_left
-            t_bottom.t_lower_left = t_left
-            t_bottom.t_upper_right = t_right
-            t_bottom.t_lower_right = t_right
+            t_bottom.t_upper_left = t_left.vid
+            t_bottom.t_lower_left = t_left.vid
+            t_bottom.t_upper_right = t_right.vid
+            t_bottom.t_lower_right = t_right.vid
 
-            t_top.t_upper_left = t_left
-            t_top.t_lower_left = t_left
-            t_top.t_upper_right = t_right
-            t_top.t_lower_right = t_right
+            t_top.t_upper_left = t_left.vid
+            t_top.t_lower_left = t_left.vid
+            t_top.t_upper_right = t_right.vid
+            t_top.t_lower_right = t_right.vid
 
             # Trecho 1.3 - Adicionar a relação inversa
             if t.t_upper_left != None :
                 if t.t_upper_left.upper_right == t:
-                    t.t_upper_left.upper_right = t_left
+                    t.t_upper_left.upper_right = t_left.vid
                 if t.t_upper_left.lower_right == t:
-                    t.t_upper_left.lower_right = t_left
+                    t.t_upper_left.lower_right = t_left.vid
 
             if t.t_lower_left != None :
                 if t.t_lower_left.upper_right == t:
-                    t.t_lower_left.upper_right = t_left
+                    t.t_lower_left.upper_right = t_left.vid
                 if t.t_lower_left.lower_right == t:
-                    t.t_lower_left.lower_right = t_left
+                    t.t_lower_left.lower_right = t_left.vid
 
             if t.t_upper_right != None :
                 if t.t_upper_right.upper_left == t:
-                    t.t_upper_right.upper_left = t_right
+                    t.t_upper_right.upper_left = t_right.vid
                 if t.t_upper_right.lower_left == t:
-                    t.t_upper_right.lower_left = t_right
+                    t.t_upper_right.lower_left = t_right.vid
 
             if t.t_lower_right != None :
                 if t.t_lower_right.upper_left == t:
-                    t.t_lower_right.upper_left = t_right
+                    t.t_lower_right.upper_left = t_right.vid
                 if t.t_lower_right.lower_left == t:                 
-                    t.t_lower_right.lower_left = t_right 
+                    t.t_lower_right.lower_left = t_right.vid
 
             # Trecho 2 - Atualizar a estrutura de busca
+
             
+            
+
+
+    def hard_case(node, segment):
+         # Trecho 1 - Atualizar o mapa trapezoidal
+         for x in node:
+
 
         else:
             assert(1)
 
-    #def hard_case(node, segment):
